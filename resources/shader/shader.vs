d74ddbfbdcbc873f861df8f548908810b8c36777
g4l3Force <q.uang123@outlook.com.vn>
2021-07-04 22:14:49 +0700

    update

--- resources/shader/shader.vs
+++ resources/shader/shader.vs
@@ -4,10 +4,13 @@ layout (location = 1) in vec2 aTexture;
 
 out vec2 Texture;
 
-uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
-   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0) * transform;
+  //Matrix multiplication is not commutative that is A⋅B≠B⋅A
+   gl_Position = projection * view * model * vec4(aPos, 1.0f);
    Texture = vec2(aTexture.x, aTexture.y);
 }
--- test/main.cpp
+++ test/main.cpp
@@ -6,19 +6,17 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
-#include <string>
-#include <fstream>
-#include <sstream>
 #define STB_IMAGE_IMPLEMENTATION
 #include "src/stb_image.h"
 #include "src/shader.h"
 
 #define RESOURCES "../resources/"
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 void initWindow(GLFWwindow*& window);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 
 int main(){
@@ -33,12 +31,52 @@ int main(){
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         std::cout<<"false to init GLAD"<<std::endl;
     }
-    
+
+    glEnable(GL_DEPTH_TEST);
+
     float vertices[] = {
-        // positions         // colors
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f,
-         0.5f, -0.5f, 0.0f,  1.0f, 1.0f,
-         0.0f,  0.5f, 0.0f,  0.5f, 0.0f
+        // positions              // texture coords
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+             0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
 
@@ -63,7 +101,7 @@ int main(){
 
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-    
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
@@ -72,8 +110,8 @@ int main(){
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
 
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 
 //    float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
 //    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
@@ -83,10 +121,10 @@ int main(){
 
     int width, height, nrChannels;
     //stbi_set_flip_vertically_on_load(true);
-    unsigned char* data = stbi_load(RESOURCES "image/test_image.png", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load(RESOURCES "image/Squares.png", &width, &height, &nrChannels, 0);
     //unsigned char* data = stbi_load(FileSystem::getPath("resource/test_image.png").c_str(), &width, &height, &nrChannels, 0);
     if (data == 0)
-        std::cout << "flase to load texture" << std::endl;
+        std::cout << "false to load texture" << std::endl;
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
@@ -94,27 +132,41 @@ int main(){
     stbi_image_free(data);
 
     while(!glfwWindowShouldClose(window)){
-        
+
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glBindTexture(GL_TEXTURE_2D, texture);
 
-        glm::mat4 transform = glm::mat4(1.0f);
-        transform = glm::translate(transform, glm::vec3(0.0f, 0.0f, -3.0f));
-        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
 
         glUseProgram(ShaderProgram);
 
-        GLuint transformLog = glGetUniformLocation(ShaderProgram, "transform");
-        glUniformMatrix4fv(transformLog, 1, GL_FALSE, glm::value_ptr(transform));
+        glm::mat4 model         = glm::mat4(1.0f);
+        glm::mat4 view          = glm::mat4(1.0f);
+        glm::mat4 projection    = glm::mat4(1.0f);
+
+        model  = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+        //trans.scale ≠ sacle.trans
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+
+        GLuint modelLoc = glGetUniformLocation(ShaderProgram, "model");
+        GLuint viewLoc = glGetUniformLocation(ShaderProgram, "view");
+        GLuint projectionLoc = glGetUniformLocation(ShaderProgram, "projection");
+        //2 different way
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));   //must include type_ptr.hpp
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, &projection[0][0]);
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
     glfwTerminate();
     return 0;
 }
@@ -133,7 +185,15 @@ void initWindow(GLFWwindow*& window){
 #endif
     //create window
     //GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "test", NULL, NULL);
-    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "test", NULL, NULL);
+    window = glfwCreateWindow(SCR_WIDTH, SCR_WIDTH, "test", NULL, NULL);
     glfwSetWindowPos(window, 30, 30);
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
 }
